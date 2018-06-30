fa10cb404926264b065e16645265e40e601bc111
elin <85221742@qq.com>
2018-06-30 21:20:57 +0800

    删除了ALLOWED_HOSTS中的所有项

--- conferenceplatform/conferenceplatform/settings.py
+++ conferenceplatform/conferenceplatform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ml33ntm)inltr3tam#o1es-&c@y5ts97l24r4(wiwetwm#k!=c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver', '0.0.0.0']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
