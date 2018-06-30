2292ad478d63e54f3745bffa2125061f99461d55
elin <85221742@qq.com>
2018-06-30 17:13:23 +0800

    haha

--- conferenceplatform/account/urls.py
+++ conferenceplatform/account/urls.py
@@ -5,9 +5,8 @@ app_name = 'account'
 urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('change_password/', views.change_password, name='change_password'),
-    path('normal_user_register/', views.normal_user_register, name='normal_user_register')
+    path('normal_user_register/', views.normal_user_register, name='normal_user_register'),
     path('organization_user_register/', views.organization_user_register, name='organization_user_register'),
     path('organization_sub_user_register/', views.organization_sub_user_register, name='organization_sub_user_register'),
-    
     
 ]
