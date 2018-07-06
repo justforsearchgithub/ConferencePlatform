30ed454926c470165300c4712a72086c0d2c9a9a
shiletong <714465499@qq.com>
2018-07-06 13:56:55 +0800

    basic of search

--- conferenceplatform/search/models.py
+++ conferenceplatform/search/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from conference.models import *
 
-# Create your models here.
+class ChunkFromConfTitle(models.Model):
+    value = models.CharField(max_length=64, primary_key=True)
+    conf_set = models.ManyToManyField(Conference)
