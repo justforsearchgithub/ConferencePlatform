6e47a6d613a1c408ae6f30e6510147f8e0d8816d
chenhengda <chd19970620@163.com>
2018-07-06 14:02:58 +0800

    修改定时发送邮件功能

--- conferenceplatform/conference/tasks.py
+++ conferenceplatform/conference/tasks.py
@@ -1,4 +1,5 @@
 from account.tasks import my_send_email
+from django.core.mail import send_mass_mail
 from celery import task
 from conference.models import *
 from account.models import *
@@ -11,12 +12,15 @@ def send_register_start_email():
     today = datetime.now()
     day = datetime.now() + timedelta(days=1)
     conferences = Conference.objects.filter(register_start__range=(today, day))
+    messages = []
     for conference in conferences:
         collect_users = conference.collect_user.all()
         for user in collect_users:
-            my_send_email(SUBJECT['register_start'],
-                          MESSAGE['register_start'].format(user.user.username, conference.title, conference.register_due),
-                          user.user.username)
+            messages.append((SUBJECT['register_start'],
+                             MESSAGE['register_start'].format(user.user.username, conference.title, conference.register_due),
+                             FROM_EMAIL,
+                             [user.user.username]))
+    send_mass_mail(messages, fail_silently=False)
     return True
 
 
@@ -25,12 +29,15 @@ def send_register_due_email():
     today = datetime.now()
     day = datetime.now() + timedelta(days=1)
     conferences = Conference.objects.filter(register_due__range=(today, day))
+    messages = []
     for conference in conferences:
         collect_users = conference.collect_user.all()
         for user in collect_users:
-            my_send_email(SUBJECT['register_due'],
-                          MESSAGE['register_due'].format(user.user.username, conference.title, conference.register_due),
-                          user.user.username)
+            messages.append((SUBJECT['register_due'],
+                             MESSAGE['register_due'].format(user.user.username, conference.title, conference.register_due),
+                             FROM_EMAIL,
+                             [user.user.username]))
+    send_mass_mail(messages, fail_silently=False)
     return True
 
 
@@ -39,12 +46,15 @@ def send_accept_due_email():
     today = datetime.now()
     day = datetime.now() + timedelta(days=1)
     conferences = Conference.objects.filter(accept_due__range=(today, day))
+    messages = []
     for conference in conferences:
         collect_users = conference.collect_user.all()
         for user in collect_users:
-            my_send_email(SUBJECT['accept_due'],
-                          MESSAGE['accept_due'].format(user.user.username, conference.title, conference.accept_due),
-                          user.user.username)
+            messages.append((SUBJECT['accept_due'],
+                             MESSAGE['accept_due'].format(user.user.username, conference.title, conference.accept_due),
+                             FROM_EMAIL,
+                             [user.user.username]))
+    send_mass_mail(messages, fail_silently=False)
     return True
 
 
@@ -53,10 +63,13 @@ def send_modify_due_email():
     today = datetime.now()
     day = datetime.now() + timedelta(days=1)
     conferences = Conference.objects.filter(modify_due__range=(today, day))
+    messages = []
     for conference in conferences:
         submissions = conference.submission_set.filter(state='M')
         for submission in submissions:
-            my_send_email(SUBJECT['modify_due'],
-                          MESSAGE['modify_due'].format(submission.submitter.user.username, conference.title, conference.modify_due),
-                          submission.submitter.user.username)
+            messages.append((SUBJECT['modify_due'],
+                             MESSAGE['modify_due'].format(submission.submitter.user.username, conference.title, conference.modify_due),
+                             FROM_EMAIL,
+                             [submission.submitter.user.username]))
+    send_mass_mail(messages, fail_silently=False)
     return True
