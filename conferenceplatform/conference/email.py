401b9be4d5948a163a009b10ebd099dd05eababb
chenhengda <chd19970620@163.com>
2018-07-06 16:51:21 +0800

    添加邮件通知功能

--- conferenceplatform/conference/email.py
+++ conferenceplatform/conference/email.py
@@ -3,11 +3,11 @@ SUBJECT = {
     'register_due': '会议注册截止通知',
     'accept_due': '会议投稿截止通知',
     'modify_due': '修改稿投递截止通知',
-    'first_passed': '恭喜',
-    'first_rejected': '很遗憾',
+    'first_passed': '恭喜，你他娘的竟然通过了评审',
+    'first_rejected': '你个菜逼你的投稿被拒绝了',
     'need_modified': '论文修改通知',
-    'modification_passed': '',
-    'modification_rejected': '',
+    'modification_passed': '恭喜，你他娘的竟然活了',
+    'modification_rejected': '你个菜逼你的修改稿被拒绝了',
 }
 
 MESSAGE = {
@@ -23,7 +23,7 @@ MESSAGE = {
                       '\n\n本邮件为系统自动发送，请勿回复！',
     'need_modified': '尊敬的{0[username]}：\n\n您向会议{0[conference]}投稿的论文{0[paper]}，现经过会议主办方的评审，评审结果如下：'
                      '您的投稿ID{0[submission]}暂未通过评审，但修改后主办方可能予以录用。\n评审意见如下：{0[advice]}\n'
-                     '请您根据评审意见进行修改，并在{modify_due}前完成修改稿提交。'
+                     '请您根据评审意见进行修改，并在{0[modify_due]}前完成修改稿提交。'
                      '\n\n本邮件为系统自动发送，请勿回复！',
     'modification_passed':  '尊敬的{0[username]}：\n\n您向会议{0[conference]}投稿的论文修改稿{0[paper]}，现经过会议主办方的评审，评审结果如下：'
                             '您的投稿ID{0[submission]}已通过评审，主办方将予以录用。在该会议开放会议注册时，我们会再次发送邮件提醒您参会。'
